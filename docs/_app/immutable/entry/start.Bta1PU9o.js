6e6a07430e8c36ef071ba410b7b0fe05f9331e8a
Philipp Wilimzig <p.wilimzig@ikusei.de>
2024-06-14 12:44:24 +0200

    page generated

---
+++ docs/_app/immutable/entry/start.Bta1PU9o.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.ezCa_ITI.js";export{t as start};
---
+++ docs/_app/immutable/nodes/2.Cp2uA2nT.js
@@ -0,0 +1,1 @@
+import{r as e}from"../chunks/index.CndXwV9T.js";import{h as r}from"../chunks/pages.BT0EJYuj.js";const o=!0;function t(){throw e(301,r)}const l=Object.freeze(Object.defineProperty({__proto__:null,load:t,prerender:o},Symbol.toStringTag,{value:"Module"}));export{l as universal};
